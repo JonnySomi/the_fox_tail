ab0bfd97d44704015a26ddc1f8fc5fc8e9fd10aa
Jonathan Somi <jonathan.somi@hotmail.com>
2022-03-01 12:58:02 +0100

    Migration for table and validations for models

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,5 @@
 class Service < ApplicationRecord
   has_many :user_services
+  validates :name, :photo_url, :type, presence: true
+  validates :type, inclusion: { in: ["Streaming", "Media", "Video Games", "Music", "Professional", "VPN"] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_111834) do
+ActiveRecord::Schema.define(version: 2022_03_01_114138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_111834) do
     t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type"
   end
 
   create_table "user_services", force: :cascade do |t|
