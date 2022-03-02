6b90f43daffd157bf9e0b8acbb64e253e9e2249a
Nominoe GERON <nominoe.geron@gmail.com>
2022-03-02 15:03:35 +0100

    Search bar in progress

---
+++ app/javascript/controllers/search_services_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ['form', 'searchInput', 'list']
+
+  connect() {
+    console.log(this.formTarget)
+    console.log(this.searchInputTarget)
+    console.log(this.listTarget)
+  }
+
+  update(event) {
+    console.log(event)
+  }
+}
