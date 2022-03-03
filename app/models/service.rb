4f6c8e7015a8ec780776d06c6777b1f383564a5c
Jonathan Somi <jonathan.somi@hotmail.com>
2022-03-03 11:08:04 +0100

    just made the validation presence for :photo :)

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
   has_many :user_services
   has_one_attached :photo
-  validates :name, :category, presence: true
+  validates :name, :category, :photo, presence: true
   validates :category, inclusion: { in: ["Streaming", "Media", "Video Games", "Music", "Professional", "VPN"] }
 end
