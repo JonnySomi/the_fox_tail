25d873ea1624094cc991a4492f85d4ce7c11b843
Nominoe GERON <nominoe.geron@gmail.com>
2022-03-03 14:37:30 +0100

    Form device designed

--- app/controllers/user_services_controller.rb
+++ app/controllers/user_services_controller.rb
@@ -3,10 +3,6 @@ class UserServicesController < ApplicationController
     @user_services = UserService.all
   end
 
-  def show
-    find_user_service
-  end
-
   def new
     @user_service = UserService.new
     @service = Service.find(params[:service_id])
